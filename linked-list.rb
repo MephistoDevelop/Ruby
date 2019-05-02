7fcb8516f61a2a437e2fc1b6e4e2f96ff91ce749
Cristian <inesherald@gmail.com>
2019-05-02 08:57:40 -0500

    ref(linked-list.rb):update main method

--- linked-list.rb
+++ linked-list.rb
@@ -7,20 +7,45 @@ class Node
   end
 end
 
-
 class LinkedList
-  attr_accessor :top, :tail
- def initialize
-  @top=nil
 
-  def push(number )
-    newnode= Node.new(number,number)
-    @top=newnode
-    puts @top
+  def initialize(val)
+    @head = Node.new(val, nil)
+  end
+
+  def add(val)
+    current = @head
+    while current.next != nil
+      current = current.next
     end
+    current.next = Node.new(val, nil)
+  end
 
+  def delete(val)
+    current.next = @head
+    if current.val = val
+      @head = current.next
+    else
+      while (current.next != nil) && (current.next.val != val)
+        current = current.next
+      end
+      unless current.next == nil
+        current.next = current.next.next
+      end
+    end
+  end
+
+  def return_list
+    elements = []
+    current = @head
+    while current.next != nil
+      elements << current
+      current = current.next
+    end
+    elements << current
   end
 end
 
-list = LinkedList.new
-list.push(20)
+lista= LinkedList.new(5)
+lista.add(3)
+lista.return_list
