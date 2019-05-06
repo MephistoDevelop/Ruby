b2ce66e18ecc748fdd109926f30ff9e65a5aa209
Cristian <inesherald@gmail.com>
2019-05-06 09:59:30 -0500

    ref(bubble_sort):change variables

--- algorithm_and_data_structure/challenge_1-1.rb
+++ algorithm_and_data_structure/challenge_1-1.rb
@@ -1,30 +1,54 @@
 class Node
-  attr_accessor :value, :next_node
+  attr_accessor :val, :next
 
-  def initialize(value, next_node = nil)
-	  @value = value
-    @next_node = next_node
+  def initialize(val, next_node)
+      @val = val
+      @next = next_node
   end
 end
 
 class LinkedList
-  attr_accessor :head, :tail
-  #setup head and tail
 
-  def add(number)
-    # your code
-    @value=Node.new(number)
-    puts @value
+  def add(val)
+    if @head == nil
+       @head = Node.new(val, nil)
+     else
+       current = @head
+    while current.next != nil
+      current = current.next
+    end
+    current.next = Node.new(val, nil)
+    end
+  end
 
+  def delete(val)
+    current.next = @head
+    if current.val = val
+      @head = current.next
+    else
+      while (current.next != nil) && (current.next.val != val)
+        current = current.next
+      end
+      unless current.next == nil
+        current.next = current.next.next
+      end
+    end
   end
 
   def get(index)
-    # your code here
+    elements = []
+    current = @head
+    while current.next != nil
+      elements << current
+      current = current.next
+    end
+    elements << current
+   return elements[index].val
+   # puts "#{elements[0].val.to_s}  :   #{elements[1].val.to_s}  :  #{elements[2].val.to_s}"
   end
 end
 
 list = LinkedList.new
-
 list.add(3)
 list.add(5)
-puts list.get(1)
+ puts list.get(1)
