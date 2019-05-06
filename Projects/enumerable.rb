503c01c6f8d29dceb62ecf6a3b79e0713f5d15c0
Cristian <inesherald@gmail.com>
2019-05-06 11:57:48 -0500

    finish(my_each):finish my_each method

--- Projects/enumerable.rb
+++ Projects/enumerable.rb
@@ -1,7 +1,20 @@
 module Enumerable
 
-    def my_each
+    def self.my_each(arr)
+     count=0
+        while (count<=arr.length) do
 
-     end
+            if( count == arr.length)
+                break;
+            else
+            puts arr[count]
+            end
+            count+=1
+        end
 
+    end
 end
+
+
+arr=["diego","marihuano","y","q",5,9,12,6]
+Enumerable.my_each(arr)
