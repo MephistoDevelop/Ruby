0238ca652608c2f9c24a55de063ab258951a4b0f
Cristian <inesherald@gmail.com>
2019-05-07 03:01:29 -0500

    fix(bubble_sort_by):finish method bubble_sort_by

--- Projects/buble_sort.rb
+++ Projects/buble_sort.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Project
    attr_accessor :arr
 
@@ -11,16 +12,31 @@ class Project
              for j in 0..@new_arr.length-1 do
               @item=@arr[i]
                @to_compare=@new_arr[j]
-                 if  @item > @to_compare
+                 if  @item < @to_compare
                         @arr[i] = @to_compare
                         @new_arr[j]=@item
                  end # end of if statement
               end #end of first for
           end #end of the second for
-          return @arr
-    end #end del metodo "todo"
+          return @arr.to_s
+    end #end del metodo "bubble_sort"
+
+    def Bubble_sort_by(arr)
+      last_check = arr.length-2
+      count = 0
+      new_arr = []
+      for i in 0..last_check
+        #puts "#{yield(arr[i], arr[i + 1])*-1} yield value : last_check = #{last_check} : i = #{i} "
+          if (yield(arr[i], arr[i + 1])*-1) > 0
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            count += 1
+            new_arr.push(arr[i])
+          end
+      end
+  return arr.reverse.to_s
+    end# end of method by
  end #end of class
 
- Arr=["hi","halo", "hey","aufwiedersen", "hello"]
  variable=Project.new
- puts variable.Bubble_sort(Arr)
+ puts variable.Bubble_sort([4,3,78,2,0,2])
+ puts variable.Bubble_sort_by(["hey", "hello", "hi"]) { |left,rigth| r=((left.length - rigth.length))   }
--- Projects/enumerable.rb
+++ Projects/enumerable.rb
@@ -1,7 +1,25 @@
 module Enumerable
 
     def my_each
+     for item in self do
+        yield  item
+    end
+end
 
-     end
+
+    def my_inject(codigo)
+
+
+      end
+
+    def multiply_els(array)
+          return  Proc.new{ |item| puts item }
+    end
+
 
 end
+
+
+arr=[2,8,2]
+puts arr.Enumerable.my_each()
+#puts arr.multiply_els(arr)
