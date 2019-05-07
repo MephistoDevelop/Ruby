83baf949d86dc10d22c567c7ad7d0066a9dddd71
Cristian <inesherald@gmail.com>
2019-05-07 11:22:44 -0500

    ref(enumrable):updated inject  method

--- Projects/enumerable.rb
+++ Projects/enumerable.rb
@@ -24,7 +24,38 @@ module Enumerable
             puts  flag==1 ? false : true
       end
 
-    def my_inject(codigo)
+      def my_any?
+        flag=false
+          self.my_each { |x|
+          yield(x) ? flag=1 : false
+        }
+        puts flag==1 ? true : false
+    end
+
+    def my_none?
+        flag=false
+        self.my_each{ |item|  yield(item) ? flag=true :  false}
+        puts flag==false ? true : false
+    end
+
+    def my_count
+      count =0
+      self.my_each{count +=1 }
+      count
+    end
+
+    def my_map
+      arr = []
+      self.my_each{|item|  arr.push(yield(item)) }
+      arr
+    end
+
+    def my_inject
+     y=yield(10,6).to_s
+     puts y
+      #                           2      x= x+item
+      self.my_each { |item|  x = yield(x,item)  }
+      puts  x
     end
 
     def multiply_els(array)
@@ -52,18 +83,53 @@ puts "Assigment 2 - Ruby Section"
 ##########################################################################
 
  #puts "Method #3 - My_select"
-puts  arr.my_select{ |x| x%2==0}.to_s
+#puts  arr.my_select{ |x| x%2==0}.to_s
 #input array = [2,8,4,14,17,27]
 # output =>[2, 8, 4, 14]
 
 ##########################################################################
 
  #puts "Method #4 - My_all?"
- arr= [2,4,14,17,nil]
-arr.my_all?{ |x|  x==nil }  #my enumerable method
-puts arr.all?  #original Enumerable Method
+ #arr= [2,4,14,17,nil]
+#arr.my_all?{ |x|  x==nil }  #my enumerable method
+#puts arr.all?  #original Enumerable Method
 
 ##########################################################################
 
+ #puts "Method #5 - My_any?"
+ #arr= [2,4,14,17,nil]
+ #arr.my_any?{|item| item == 17}
+#puts arr.any?(17)
+
+##########################################################################
+
+#puts "Method #6 - My_none?"
+#arr= [2,4,14,17,nil]
+#arr.my_none?{ |x| x==nil  }
+#puts arr.none?(nil)
+
+##########################################################################
+
+#puts "Method #7 - My_count?"
+#arr= [2,4,14,17,nil]
+#puts arr.my_count
+#puts arr.count
+
+##########################################################################
+
+
+#puts "Method #8 - My_map"
+#arr= [2,4,14,17,nil]
+#puts arr.my_map{ |x| x.to_s+ "!" }.to_s
+#puts arr.map{|x| x.to_s + "!" }.to_s
+
+##########################################################################
+
+puts "Method #9 - My_inject"
+arr=[2,3,5,12,8,1]
+ arr.my_inject{ |sum,x|  sum += x }
+puts arr.inject { |sum,x| sum += x }
+
+##########################################################################
 
 #puts arr.multiply_els(arr)
