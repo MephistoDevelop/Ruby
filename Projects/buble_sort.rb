674febfb8152e84d8efbfae52aa79c265e199884
MephistoDevelop <inesherald@gmail.com>
2019-05-08 16:01:29 -0500

    Update buble_sort.rb

--- Projects/buble_sort.rb
+++ Projects/buble_sort.rb
@@ -9,24 +9,23 @@
                  if  item < to_compare
                         arreglo[i] = to_compare
                         new_arr[j]=item
-                 end # end of if statement
-              end #end of first for
-          end #end of the second for
+                 end 
+              end 
+          end 
           return arreglo.to_s
-    end #end del metodo "bubble_sort"
+    end 
 
     def Bubble_sort_by(arr)
       last_check = arr.length-2
       count = 0
       for i in 0..last_check
-        #puts "#{yield(arr[i], arr[i + 1])*-1} yield value : last_check = #{last_check} : i = #{i} "
           if (yield(arr[i], arr[i + 1])*-1) > 0
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
             count += 1
           end
       end
   return arr.reverse.to_s
-    end# end of method by
+    end
 
 puts Bubble_sort([4,3,78,2,0,2])
  # => [0, 2, 2, 3, 4, 78]
