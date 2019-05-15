8906bc2863e7965fb9a105dc5029ad30df77d2e2
Cristian <inesherald@gmail.com>
2019-05-15 01:13:39 -0500

    ref(Code_review):fixed issues from code review

--- Projects/enumerable.rb
+++ Projects/enumerable.rb
@@ -33,7 +33,7 @@ end
     def my_none?
         flag=false
         self.my_each{ |item|  yield(item) ? flag=true :  false}
-        puts flag==false ? true : false
+         flag==false ? true : false
     end
 
     def my_count
@@ -76,86 +76,11 @@ end
            else
            x = yield(x,item)
            end      }
-       puts x
+        x
    end
 
     def multiply_els(array)
-     puts array.my_inject{ |num,x|  num *= x }
+      array.my_inject{ |num,x|  num *= x }
     end
 
 end
-
-##########################################################################
-
-#puts "Method #1 - My_each "
-#puts arr.my_each{ |item| item}.to_s
-
-##########################################################################
-
-#puts "Method #2 - My_each_with_index "
-#My_each_function wth index
-#arr.my_each_with_index{ |val,i| puts "Value :#{val} is on the position: #{i} "}
-
-##########################################################################
-
-#puts  arr.my_select{ |x| x%2==0}.to_s
-#input array = [2,8,4,14,17,27]
-# output =>[2, 8, 4, 14]
-
-##########################################################################
-
-puts "Method #4 - My_all?"
- arr= [2,4,14,17]
-puts arr.my_all?{ |x|  x> 10 }  #my enumerable method
-puts arr.all?  #original Enumerable Method
-
-##########################################################################
-
-#puts "Method #5 - My_any?"
-#arr= [2,4,14,17,nil]
-#puts  arr.my_any?{|item| item == 17}
-#puts arr.any?(17)
-
-##########################################################################
-
-#puts "Method #6 - My_none?"
-#arr= [2,4,14,17,nil]
-#arr.my_none?{ |x| x==nil  }
-#puts arr.none?(nil)
-
-##########################################################################
-
-#puts "Method #7 - My_count?"
-#arr= [2,4,14,17]
-#puts arr.my_count
-#puts arr.my_count{|i|  i > 3 }
-#puts arr.count
-
-##########################################################################
-
-#puts "Method #8 - My_map"
-#arr= [2,4,14,17,nil]
-#puts arr.my_map{ |x| x.to_s+ "!" }.to_s
-#puts arr.map{|x| x.to_s + "!" }.to_s####
-
-######################################################################
-
-#puts "Method #9 - My_inject"
-#arr=[2,3,5,12,8,1]
-#arr.my_inject{ |sum,x|  sum += x }
-#puts arr.inject { |sum,x| sum+= x }
-
-##########################################################################
-
-#puts "Method #10 - Multiply_els"
-#arr=[2,4,5]
-#puts arr.multiply_els(arr)
-# => 40
-
-##########################################################################
-
-#puts "Method #11- Map with Proc"
-#arr=[2,3,5,12,8,1]
-#puts arr.my_map(&(Proc.new  {|x | x ** 2 }))
-
-##########################################################################
