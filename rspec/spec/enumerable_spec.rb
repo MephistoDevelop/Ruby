bf10ab40d8eb21a470340408ec06ac40fdb72917
Cristian <inesherald@gmail.com>
2019-05-15 04:13:52 -0500

    fix(#my_each):fixed issues from sintax

--- rspec/spec/enumerable_spec.rb
+++ rspec/spec/enumerable_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Enumerable do
     describe "#my_each" do
         it "select elements that are major than 10" do
           puts "importada la wea"
-puts "#{[2,8,4,14,17,27].my_each? { |x|  x >10} } esto es string"
-         #  expect([2,8,4,14,17,27].my_each? { |x|  x >10}).to eql([14,17,27])
+           expect([2,8,4,14,17,27].my_each?{|x| x}).to eql([2,8,4,14,17,27])
         end
     end
 end
